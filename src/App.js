13e211ef4232910aa0d05173b08a8220fb7edc08
NeroShredder <41788041+NeroShredder@users.noreply.github.com>
2019-11-05 11:53:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+
+
+class App extends Component {
+  constructor (props) {
+      super(props);
+      this.state = {
+          items: [],
+          isLoaded: false,
+      }
+  }
+
+  componentDidMount() {
+
+      fetch('https://cors-anywhere.herokuapp.com/https://api.meteo.lt/v1/places/kaunas/forecasts/long-term')
+      .then(res =>res.json())
+      .then (json => {
+          this.setState({
+              isLoaded: true,
+              items: json,
+          })
+
+      });
+
+  }
+
+  render() {
+
+      var { isLoaded, items } = this.state;
+      if (!isLoaded) {
+          return <div>Kraunasi...</div>;
+      }
+      // Pasileidzia, tik tada, kai json is api uzkrautas, bet ismeta klaida "items.map is not a function"
+      else {
+      return (
+          <div className="App">
+              Duomenys ikelti
+              <Header/>
+              <Nav/>
+              <Main/>
+              <Footer/>
+              
+              <ul>
+                {items.map(item => (
+                  <li key={item.forecastTimestamps}>
+                    Data ir laikas: {item.forecastTimeUtc} | Oro temperatura: {item.airTemperature} | Prognoze: {item.conditionCode}
+                  </li>
+                ))};
+              </ul>
+          </div>
+
+          
+      );
+      }
+  }
 }
+
+
+
 
 export default App;
